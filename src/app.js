0928f673f7e4bbc2fa58044808fbee2fe1c46b5c
JulianoV <ventolajuliano@gmail.com>
2020-03-09 18:53:30 -0300

    Arquivos base server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  // Cria o servidor
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // Habilita o server trabalhar com Json
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ src/server.js
@@ -0,0 +1,6 @@
+import app from './app';
+
+// Start backend service on port 3333
+app.listen(3000, () => {
+  console.log('Milvus server running on port 3000');
+});
